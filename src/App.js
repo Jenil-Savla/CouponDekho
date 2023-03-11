cf846641a725647646ee11221232d3a8f0c824b6
khushi3114 <khushidjobanputra77@gmail.com>
2023-03-11 15:21:58 +0530

    genrate form made

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import { Route, Routes } from 'react-router-dom';
+import Generate from './components/Generate';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Generate />
       <Routes>
         {/* <Route path='/' element={<Home/>}/> */}
         <Route path='/signup' element={<Signup/>} />
